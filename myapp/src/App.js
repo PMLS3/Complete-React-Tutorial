8b5e8583ccd6b22253ecacc045b4a235473a26aa
PMLS3 <peet@partnersinbiz.tech>
2020-10-18 17:51:35 +0200

    setup

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Ninjas from "./Ninjas";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My First React App!</h1>
+      <p>Welcome :)</p>
+      <Ninjas />
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/Ninjas.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Ninjas() {
+  return (
+    <div>
+      <div>Name: Ryu</div>
+      <div>Age: 30</div>
+      <div>Belt: Black</div>
+    </div>
+  );
+}
